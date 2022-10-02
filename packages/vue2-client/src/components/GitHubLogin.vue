f949d0c9db4bcda4570690b7da9839bdec37e40c
Kyle Finley <kyle@kylefinley.net>
2022-10-02 15:02:03 +0000

    update: GitHub login testing... set env vars

--- packages/vue2-client/src/components/GitHubLogin.vue
+++ packages/vue2-client/src/components/GitHubLogin.vue
@@ -40,28 +40,28 @@ export default class GitHubLogin extends Vue {
     return `${rootURl}?${qs.toString()}`
   }
 
-  login() {
-    // Initializes OAuth.io with API key
-    // Sign-up an account to get one
-    window.OAuth.initialize('01811fceddc77dd32903') //TODO: MOVE THIS!!
+  // login() {
+  //   // Initializes OAuth.io with API key
+  //   // Sign-up an account to get one
+  //   window.OAuth.initialize('01811fceddc77dd32903') //TODO: MOVE THIS!!
 
-    // Popup and ask for authorization
-    window.OAuth.popup('github').then((github) => {
-      console.log('github:', github)
-      // Prompts 'welcome' message with User's name on successful login
-      // #me() is a convenient method to retrieve user data without requiring you
-      // to know which OAuth provider url to call
-      github.me().then((data) => {
-        console.log('data: ', data)
-        alert('Your Github email: ' + data.email + '.\nCheck console logs for more info.')
-      })
+  //   // Popup and ask for authorization
+  //   window.OAuth.popup('github').then((github) => {
+  //     console.log('github:', github)
+  //     // Prompts 'welcome' message with User's name on successful login
+  //     // #me() is a convenient method to retrieve user data without requiring you
+  //     // to know which OAuth provider url to call
+  //     github.me().then((data) => {
+  //       console.log('data: ', data)
+  //       alert('Your Github email: ' + data.email + '.\nCheck console logs for more info.')
+  //     })
 
-      // You can also call Github's API using #.get()
-      github.get('/user').then((data) => {
-        console.log('self data:', data)
-      })
-    })
-  }
+  //     // You can also call Github's API using #.get()
+  //     github.get('/user').then((data) => {
+  //       console.log('self data:', data)
+  //     })
+  //   })
+  // }
 }
 </script>
 
