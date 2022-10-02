454b76502132e4c65c2b6c88636070d71845608c
Kyle Finley <kyle@kylefinley.net>
2022-10-02 15:22:59 +0000

    update: GitHub login testing... added cors policy to s3 bucket

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -42,7 +42,7 @@ export class InfrastructureStack extends Stack {
           allowedMethods: [
             s3.HttpMethods.GET,
           ],
-          allowedOrigins: ['https://github.com'],
+          allowedOrigins: ['*'],
           allowedHeaders: ['*'],
         },
       ],
