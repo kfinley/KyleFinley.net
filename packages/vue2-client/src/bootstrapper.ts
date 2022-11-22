6bb600339a6077a748e91c91dd724d3a0bcfe135
Kyle Finley <kyle@kylefinley.net>
2022-11-22 01:55:13 +0000

    update: added GitHub-Commands bootstrapper to vue2-client bootsrapper

--- packages/vue2-client/src/bootstrapper.ts
+++ packages/vue2-client/src/bootstrapper.ts
@@ -1,14 +1,12 @@
 import { container } from './inversify.config';
-// import { bootstrapper as ghCommandsBootstrapper } from "@kylefinley.net/github-commands/src/";
+import { bootstrapper as ghCommandsBootstrapper } from "@kylefinley.net/github-commands/src/";
 import { Store } from "vuex";
 
 export default function bootstrapper(store: Store<any>) {
 
   console.log('Bootstrapper', process.env.NODE_ENV);
 
-  // setupModules(store);
-
-  // ghCommandsBootstrapper(container);
+  ghCommandsBootstrapper(container);
 
   console.log("Bootstrapper Done");
 
