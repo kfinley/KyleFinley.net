39d57539b4119d289b581f29dd247806ef73fa52
Kyle Finley <kyle@kylefinley.net>
2022-12-09 17:05:20 -0800

    update: pulling last modified date from meta json file if it exists for sitemap lastmod value

--- infrastructure/scripts/generate-sitemap.sh
+++ infrastructure/scripts/generate-sitemap.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+get_meta_tag() {
+  local value=$(PROPERTY_NAME=$1 jq '.metaTags[] | select(.property==env.PROPERTY_NAME) | .content' $2)
+  value=`sed -e 's/^"//' -e 's/"$//' <<<"$value"`
+  echo $value
+}
+
 create_url() {
   file="${2/json/md}"
   if [ ! -f "$file" ]; then
     file="${2/json/vue}"
   fi
-  local last_mod=$(date -r $file "+%Y-%m-%d")
-  local url=$(jq '.metaTags[] | select(.property=="og:url") | .content' $2)
+
+  local last_mod=$(get_meta_tag "article:modified_time" $2)
+
+  if [ "$last_mod" = "" ]; then
+    last_mod=$(date -r $file "+%Y-%m-%d")
+  fi
+
+  parts=($last_mod) # if the date string has time we just want the first segment which is the date
+  last_mod=${parts[0]}
+
+  if [[ "$last_mod" == *"/"* ]]; then
+    last_mod=$(date -j -f '%m/%d/%Y' "$last_mod" +'%Y-%m-%d')
+  fi
+
+  local url=$(get_meta_tag "og:url" $2)
   cat >>$1 <<EOF
   <url>
     <loc>$url</loc>
