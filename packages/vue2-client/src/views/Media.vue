e0b4be9add53306c6884a8f2a3152250dd484417
Kyle Finley <kyle@kylefinley.net>
2023-05-28 15:31:03 -0700

    update: added WSJC Ula 5/25 audio recordings

--- packages/vue2-client/src/views/Media.vue
+++ packages/vue2-client/src/views/Media.vue
@@ -7,6 +7,10 @@
     <audio-player name="West Side Jazz Club" location="Ula SF - Set 2" date="05/26/2023" g-id="1Qt_6oPJ6XeMwiXawXGmqDIWpCiReF0ab" />
     <audio-player name="West Side Jazz Club" location="Ula SF - Set 3" date="05/26/2023" g-id="1wtpffXwRkWwSfB5dzXTlHDDOUSIiyEWb" />
     <audio-player name="West Side Jazz Club" location="Ula SF - Set 4" date="05/26/2023" g-id="119up4SgyO2OpiZQ9c_w8jCp7Td0Kt8UL" />
+    <audio-player name="West Side Jazz Club" location="Ula SF - Set 1" date="05/25/2023" g-id="1AOeBwaRbngL7pbrDPqb5jG1U1NkukvFH" />
+    <audio-player name="West Side Jazz Club" location="Ula SF - Set 2" date="05/25/2023" g-id="1wsZQNp77psnMo_8iCMtLIq19EesvHef9" />
+    <audio-player name="West Side Jazz Club" location="Ula SF - Set 3" date="05/25/2023" g-id="1BKpgkkGGxj-vO7K53EgLZHkZ_RR3haaq" />
+    <audio-player name="West Side Jazz Club" location="Ula SF - Set 4" date="05/25/2023" g-id="1YvWkERAdpXE-iBQVzgp9YScwyYegtMAd" />
     <audio-player name="The Cody Sisters" location="Amado's SF" date="5/24/2023" g-id="18Q_gKk0o6w5qShoOBK83kCQuLzx9a5oj" />
   </div>
 </template>
