b0d32266ead52a650e462aad37b80f4be49a391d
Kyle Finley <kyle@kylefinley.net>
2023-08-21 09:31:15 -0700

    update: music page copy

--- packages/vue2-client/src/views/Music.vue
+++ packages/vue2-client/src/views/Music.vue
@@ -59,45 +59,59 @@
 
     <h4 class="text-center">My Musical Journey</h4>
     <p>
-      I've been playing music now for over 30 years. I grew up as a typical band kid hiding out in the band hall with the other cool kids.
-      I'm lucky and come from good stock of musical talent so playing music came pretty naturally for me. When I was 12 I joined beginner
-      band at school and picked the trombone because that's what my dad played. He let me use his Bach Stradivarius he bought in high school
-      after he quit playing football due to a serious game injury. I played his horn through high school and college, then it was almost
-      lost in a house fire so he asked for it back to hold for safe keeping. Luckily I now have my father's horn back and I'm actively
-      chopping wood in Golden Gate Park with the goal of playing my first public shows soon!
+      I've been playing music now for over 30 years. I grew up hiding out in the band hall with the other cool kids playing lots of stuff.
+      When I was 11 I joined beginner band at school. I picked the trombone because that's what my dad played. He let me use his Bach
+      Stradivarius he bought in high school after he quit playing football due to a serious game injury. I'm lucky and come from good stock
+      of musical talent so playing music came pretty naturally for me. I played the trombone through high school and college, then it was
+      almost lost in a house fire so he asked for it back to hold for safe keeping. Luckily I now have my father's horn back and I'm
+      actively chopping wood in Golden Gate Park with the goal of playing my first public shows soon!
     </p>
     <p>
-      I started playing bass because of people like Cliff Burton, Les Claypool, and Flea. When I was about 13 I got a cheap electric bass
-      guitar (and an even cheaper garage sale acoustic guitar). I started taking lessons from my friend and band director
+      When I was about 13 I got a cheap electric bass guitar (and an even cheaper garage sale acoustic guitar). I started playing bass
+      because of people like Cliff Burton, Les Claypool, Jeff Ament, and Flea. I started taking lessons from my friend and band director
       <a href="http://jeffmadlock.com" target="Jeff">Jeff Madlock</a> and he started grooming me for high school jazz band. I continued with
-      guitar, and still play it a lot, but after moving to upright bass in early high school I've considered myself primarily a bass player.
+      guitar, and still play it a lot, but since moving to upright bass in early high school I've considered myself primarily a bass player.
+      When it comes to bass I'm most influenced by Ray Brown, Paul Chambers, Oscar Pettiford, Ron Carter, Christian McBride, and of course
+      Jimmy Blanton.
     </p>
     <p>
-      In high school I was a 3 time All-State Jazz Band member from it's inaugural year through graduation. I also made All-State Orchestra
-      on Bass without ever playing in an orchestra before and performed in both the Jazz and Orchestra bands the same weekend. I received
-      both the John Philip Sousa and Louis Armstrong Jazz Award my senior year. I also attended the Jamey Aebersold Jazz Camp in 1995 which
-      was where I learned how to walk bass lines from Jamey Aebersold himself (the dude played everything).
+      In high school I was a 3 time All-State Jazz Band member, from it's inaugural year through graduation. I also made All-State Orchestra
+      on Bass my senior year without ever playing in an orchestra before and performed in both the Jazz and Orchestra bands the same
+      weekend. I received both the John Philip Sousa and Louis Armstrong Jazz Award my senior year. I attended the Jamey Aebersold Jazz Camp
+      in 1995 which was where I learned how to walk bass lines from Jamey Aebersold himself (the dude played everything). I also attended
+      Arkansas Governors School in the Music Area in 1997. Governors School was a great experience for me. I learned a lot! It's a real
+      shame that Ava Hutchinson felt compelled to destroy the program for no good reason other than blind ignorant christian conservatism.
+      The destruction of program was a true loss for the Arkansas education system and more importantly took away a learning opportunity for
+      smart kids and gifted artists in the state.
     </p>
     <p>
       I took music pretty seriously through the end of high school was set to attend the University of North Florida with a full ride for
       jazz studies but for various reasons decided to focus on tech stuff in college and ended up starting at the University of North Texas.
       I bounced around schools a bit and landing at the University of Colorado where I played in one of the CU Jazz Combos under Chip
-      Stephens. Unfortunately I let music fall to the back burner after school but I got serious about it again several years ago and I'm
-      now out of the woodshed and back on the stage!
+      Stephens.
     </p>
     <p>
-      After an overpriced botched overhaul in 2012 of my bass I was so upset I couldn't even play it and it sadly sat in the corner for
-      another 6 years. (Don't ever use or trust Upton... just saying.) After randomly meeting a Berklee Comp kid while kiteboarding at the
-      beach in Boston, he invited me to play bass at a bluegrass jam in Jamaica Plain. I thought it'd be a good place to get my chops back
-      so I started playing with them weekly. They're a great crew and lately they play Sundays at the Turtle Creek Brewery or at the JP pond
-      in the summer.
+      Unfortunately I let music fall to the back burner after school while I was working as a traveling consultant. I tried to get back to
+      playing around 2012 but after an extremely overpriced botched overhaul of my bass I was so upset I couldn't even play it. Sadly it sat
+      in the corner for another 6 years or so before I started playing seriously again. (Don't ever trust or use Upton... just sayin'.)
+      Getting back to playing bass was interesting, I randomly meet a Berklee Comp kid while kiteboarding at the beach in Boston and he
+      invited me to join him at a bluegrass jam in Jamaica Plain. I thought why not! Bluegrass is easy enough right? That should be a good
+      place to get my chops back! So I started playing bass with them weekly. They're a great crew and lately they play Sundays at a bar or
+      brewery in Jamaica Plain or at the JP pond in the summer.
     </p>
     <p>
-      I landed in SF in the summer of 2019 and found The Lucky Horseshoe weekly bluegrass jam run by Frank Holmes my first week in town. I
-      was the regular bass player for the Shoe Jam through the pandemic and into it's transition to a new leader. Now I still play the jam
-      most weeks but I don't play the full 3 hour gauntlet anymore! I started focusing on jazz in the last few years and these days I'm
-      playing in a few groups around SF and I host a monthly Jazz Jam at The Lucky Horseshoe in Bernal Heights every 3rd Wednesday of the
-      month.
+      In the summery of 2019 I moved across the country and landed in SF. I found The Lucky Horseshoe weekly bluegrass jam run by Frank
+      Holmes my first week in town. I was the regular bass player for the Shoe Jam for almost 4 years, from July 2019 through the pandemic
+      and into it's transition to a new leader (roughly May 2023). I still drop by and play at the jam most weeks but I don't play the full
+      3 hour gauntlet anymore!
+    </p>
+    <p>
+      I started focusing on jazz again as my primary genre a few years back and these days I'm playing around SF with a few groups. I host a
+      monthly Open Jazz Jam at <a href="https://www.theluckyhorseshoebar.com/" target="Shoe">The Lucky Horseshoe</a> every 3rd Wednesday of
+      the month. Sometimes we have impromptu "Overtime" jazz jams at the Shoe after the Bluegrass Jam on Sunday nights if we have jazz heads
+      there. You can also catch me there on random Monday or Tuesdays with a guitar player or other instruments playing jazz. I've been
+      working with the bartenders to turn the Shoe into a solid low key jazz room and we're starting to get some real traction! Feel free to
+      hit me up if you have a jazz group and want to book at night at the Shoe.
     </p>
     <p>
       I'm available for hire and looking for players to form both a regular gigging jazz group and a progressive bluegrass / Americana
