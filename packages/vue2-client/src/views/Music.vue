fcd128dbfe8c877b48bf67c27989d73267a5edc2
Kyle Finley <kyle@kylefinley.net>
2023-10-09 07:15:51 -0700

    update: music page

--- packages/vue2-client/src/views/Music.vue
+++ packages/vue2-client/src/views/Music.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center">My Music</h1>
     <div class="text-center">
       <p>
-        <i>Jazz, Bluegrass, and Americana upright bassist currently slappin' strings around the San Francisco Bay area. </i>
+        <i>Jazz, Bluegrass, and Americana upright bassist currently slappin' strings around the San Francisco Bay area! </i>
       </p>
       <p>Recordings can be found on my <br /><router-link to="/media">Media Page</router-link></p>
     </div>
@@ -21,19 +21,19 @@
         <a href="https://zacksanders.bandcamp.com/album/standards-trio-live-in-sf" target="live"
           >our live album recorded at Le Petit Paris 75</a
         >
-        for a sample of our sound
-      </li>
-      <li>
-        <em><a href="http://westsidejazzclub.com" target="new">West Side Jazz Club</a></em> - This group is run by Scott Silverberg on Tenor
-        Sax and Vocals. Gigs around the Bay Area
-      </li>
-      <li>
-        <em><a href="https://www.thebash.com/bluegrass/nobodyfromnashville" target="new">Nobody From Nashville</a></em> - Band for hire!
-        Four piece featuring Joan Wilson Rueter on fiddle, Ian Epstein on guitar, and Steve Owen on dobro
+        for a sample of our sound.
       </li>
       <li>
         <em>Angel Island</em> - We play jazz standards for the community and tourists in the park at the Conservatory of Flowers Tunnel.
         This group has been playing in GG park for several years. We also play other gigs when we can get them.
+      </li>
+      <li>
+        <em><a href="http://westsidejazzclub.com" target="new">West Side Jazz Club</a></em> - This group is run by Scott Silverberg on Tenor
+        Sax and Vocals. Gigs around the Bay Area.
+      </li>
+      <li>
+        <em><a href="https://www.thebash.com/bluegrass/nobodyfromnashville" target="new">Nobody From Nashville</a></em> - Band for hire!
+        Four piece featuring Joan Wilson Rueter on fiddle, Ian Epstein on guitar, and Steve Owen on dobro.
       </li>
       <li>
         <em><a href="https://www.fakemelody.com" target="fakemelody">Fake Melody</a></em> - Now and then I'll fill in with this SF based
@@ -41,9 +41,17 @@
       </li>
       <li>
         <em><a href="https://www.gigsalad.com/mission_hill_san_francisco" target="new">Mission Hill</a></em> - Once in a blue moon I'm
-        privileged to fill in on bass with Aaron & Richard playing originals and classic Americana/Bluegrass
+        privileged to fill in on bass with Aaron & Richard playing originals and classic Americana/Bluegrass.
       </li>
-      <li><em>Open Jazz Jam @ The Lucky Horseshoe</em> - Every 3rd Wednesday of the Month 8-11pm</li>
+      <li>
+        <em>DKE</em> - Daisy, Kyle, &amp; Eli. Old school jazzy folky funky fun with Daisy on guitar, Eli on drums, &amp; me on bass. We
+        played together through the spring of 2023 before Daisy went back to being a world traveler.
+      </li>
+      <li>
+        <em>Open Jazz Jam @ The Lucky Horseshoe</em> - I run this jam at the Shoe ever 3rd Wednesday of the Month 8-11pm. We aim to have
+        this as the chillest jam in town. The Dirty Works trio is the core house band and we always have great group of players come
+        through.
+      </li>
       <li>
         <em>Sunday Bluegrass Jam @ <a href="https://www.theluckyhorseshoebar.com/" target="Shoe">The Lucky Horseshoe</a></em>
         - This was a great jam when Frank Holmes ran it and I enjoyed playing there every week for several years. I don't go anymore due to
@@ -53,21 +61,20 @@
         gone. Now many of the older top notch players won't come anymore since the new leader took over.
       </li>
       <li>
-        <em>Showcase Bluegrass Jam @ The Lucky Horseshoe</em> - 1st Sunday of the month from 8-9:30pm (currently in hiatus but hopefully
-        back soon!)
+        <em>Showcase Bluegrass Jam @ The Lucky Horseshoe</em> - 1st Sunday of the month from 8-9:30pm. Currently in hiatus but hopefully
+        back soon!
       </li>
       <li><em>The Hotel Utah Saloon Weekly Bluegrass Jam</em> - Every Tuesday from 7pm 'til done</li>
       <li><em>Woody Lauer's Bluegrass Jam @ Amado's</em> - Every Wednesday from 7-10pm</li>
       <li>
         <em><a href="https://goldengatejams.com" target="new">Community Music Hangout</a></em> - Weekends from 11am-2pm in Golden Gate Park
-        near Stowe Lake on JFK
-      </li>
-      <li>
-        <em>DKE</em> - Daisy, Kyle, &amp; Eli. Old school jazzy folky funky fun with Daisy on guitar, Eli on drums, &amp; me on bass. We
-        played together through the spring of 2023 before Daisy went back to being a world traveler
+        near Stowe Lake on JFK.
       </li>
       <li><em>Ocean Ale House Jazz Jam</em> - Weekly Jazz Jam on Wednesday Nights in Monterey Heights</li>
-      <li><em>Madrone Jazz Jam</em> - Weekly Jazz Jam on Sunday nights at Madrone Art Bar in Alamo Square</li>
+      <li>
+        <em>Madrone Jazz Jam</em> - Weekly Jazz Jam on Sunday nights at Madrone Art Bar in Alamo Square. I don't go to this one very often
+        and prefer having overtime jazz at the Shoe on Sundays if possible.
+      </li>
     </ul>
 
     <h4 class="text-center">My Musical Journey</h4>
