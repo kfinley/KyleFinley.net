b8fd0484fce439ae648e01dc1b52c2ff07f63008
Kyle Finley <kyle@kylefinley.net>
2023-11-15 15:41:36 -0800

    update: added recordings

--- packages/vue2-client/src/views/Media.vue
+++ packages/vue2-client/src/views/Media.vue
@@ -21,21 +21,111 @@ export default class Music extends Vue {
   get tracks() {
     return [
       {
+        title: "Monday Jazz Jam - Set 1",
+        location: "Waystone - SF, CA",
+        date: "11/13/2023",
+        id: "1ImnGsPQnFrSpi_0_Ono-qhCuR1Me2gQN"
+      },
+      {
+        title: "Monday Jazz Jam - Set 2",
+        location: "Waystone - SF, CA",
+        date: "11/13/2023",
+        id: "1nwq0Mjq5pMjCKyJ4-00Me5hGsYvFBcX9"
+      },
+      {
+        title: "Monday Jazz Jam - Set 1",
+        location: "Waystone - SF, CA",
+        date: "11/6/2023",
+        id: "1uQ7cqch4nwfdTHuwwffpT3DeYKOOHN7J"
+      },
+      {
+        title: "Monday Jazz Jam - Set 2",
+        location: "Waystone - SF, CA",
+        date: "11/6/2023",
+        id: "1YQAHS_ZJ_FflcIH5YteQoJJsuACS4oRQ"
+      },
+      {
+        title: "Monday Jazz Jam - Set 3",
+        location: "Waystone - SF, CA",
+        date: "11/6/2023",
+        id: "1YYROim7F_p6VqDLz2Rj6HQ69bmgFedKn"
+      },
+      {
+        title: "Fake Melody Trio - Set 1",
+        location: "Ivory & Vine - SF, CA",
+        date: "10/27/2023",
+        id: "1aYzatGfhjB43e7t7pOHfZGjX7jbvwJAF"
+      },
+      {
+        title: "Fake Melody Trio - Set 2",
+        location: "Ivory & Vine - SF, CA",
+        date: "10/27/2023",
+        id: "1fSXoGfCnCVQrIJRNqUGm2jocr90oS6fK"
+      },
+      {
+        title: "Shoe Jam - Set 1",
+        location: "The Lucky Horseshoe - SF, CA",
+        date: "9/20/2023",
+        id: "1WtrgM1AGpNquvnkzCr1LQm1rcyTu3IbO"
+      },
+      {
+        title: "Shoe Jam - Set 2",
+        location: "The Lucky Horseshoe - SF, CA",
+        date: "9/20/2023",
+        id: "11yqPLaw0q_hxRDkJAqEs9JivzOrY7CMQ"
+      },
+      {
+        title: "Overtime Jazz Jam - Set 1",
+        location: "The Lucky Horseshoe - SF, CA",
+        date: "7/9/2023",
+        id: "1MRZatxHFzORqq2OErVsmVl35XnS90S4g"
+      },
+      {
+        title: "Overtime Jazz Jam - Set 2",
+        location: "The Lucky Horseshoe - SF, CA",
+        date: "7/9/2023",
+        id: "1LneWfZku7cGuFXsGSB4JSVThwgfA4DD3"
+      },
+      {
+        title: "Zack & Kyle - Set 1",
+        location: "Waystone - SF, CA",
+        date: "7/1/2023",
+        id: "1Ckuzpowl2RlvEDUsoyXmrP9_dfgiflwk"
+      },
+      {
+        title: "Zack & Kyle - Set 2",
+        location: "Waystone - SF, CA",
+        date: "7/1/2023",
+        id: "1I72tGe3DjT24W2zeikKZu_msC42vMPP7"
+      },
+      {
+        title: "Shoe Jam - Set 1",
+        location: "The Lucky Horseshoe - SF, CA",
+        date: "6/21/2023",
+        id: "1X34fVUm6WNwMFvsJaEfXJmdRAOrEH7Gm"
+      },
+      {
+        title: "Shoe Jam - Set 2",
+        location: "The Lucky Horseshoe",
+        date: "6/21/2023",
+        id: "1-6xPClYO2JRSAQ6_zg5BphGiWorhO8J5"
+      },
+      {
         title: "June MusicLuck - Bangers",
         location: "The Mission SF, CA",
-        date: "06/14/2023",
+        date: "6/14/2023",
         id: "11f0VXqVZO9osBK39hqr8C9CWJ8kTy50l"
       },
       {
         title: "June MusicLuck - Ballads",
         location: "The Mission SF, CA",
-        date: "06/14/2023",
+        date: "6/14/2023",
         id: "162XA7oEBatYdUMfC6yjmTTcHohM8SMjE"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 1",
-        date: "06/09/2023",
+        date: "6/9/2023",
         id: "12NU6WDzq9MbcBWvamAGD_Aj3CIFTJc9S"
       },
       {
@@ -47,83 +137,82 @@ export default class Music extends Vue {
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 3",
-        date: "06/09/2023",
+        date: "6/9/2023",
         id: "1u7txq6LIDRxur_w9jf4Mjl8KE1wKLsKm"
       },
       {
         title: "Nobody From Nashville",
         location: "Morgan Hill Birthday - Set 1",
-        date: "06/03/2023",
+        date: "6/3/2023",
         id: "1t58ThL5TqsxDLwxZv1KXG0ph8LaYFyhz"
       },
       {
         title: "Nobody From Nashville",
         location: "Morgan Hill Birthday - Set 2",
-        date: "06/03/2023",
+        date: "6/3/2023",
         id: "1zPlx3ooaluX90hYm4V282XfVeA4Qtjm1"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 1",
-        date: "06/02/2023",
+        date: "6/2/2023",
         id: "1IBiKf_IhJEDO8Lp-1boseWEg7XJsH1s4"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 2",
-        date: "06/02/2023",
+        date: "6/2/2023",
         id: "1JM_BKd2s2Cr-ULQCdHJonZXrtRJ_qpOo"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 3",
-        date: "06/02/2023",
+        date: "6/2/2023",
         id: "1OV01BPNcqfVnMbmNZarAT2y3Vocr1hQS"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 4",
-        date: "06/02/2023",
+        date: "6/2/2023",
         id: "1_uEOZUDf8FdmN3bYRtnSO8YkL1MYf4x-"
       },
-
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 1",
-        date: "06/01/2023",
+        date: "6/1/2023",
         id: "1hSf9NWxZ2VsoJwh1zu-GDestvLevQYR1"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 2",
-        date: "06/01/2023",
+        date: "6/1/2023",
         id: "177tMViSyOtiN1WAPKGZoJrDzBdM4l-1K"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 3",
-        date: "06/01/2023",
+        date: "6/1/2023",
         id: "15-ZrEU4sqGqofUC1QIHf2tsUPoa0Xx-U"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 4",
-        date: "06/01/2023",
+        date: "6/1/2023",
         id: "10vaABQN0EePFVyvBhUiQyIPMf7y8k2JW"
       },
       {
         title: "West Side Jazz Club",
         location: "Ula SF - Set 1",
-        date: "05/26/2023",
+        date: "5/26/2023",
         id: "1okP4LV1UKT10E-8liSmG-vIq-RPc2HkW"
       },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 2", date: "05/26/2023", id: "1Qt_6oPJ6XeMwiXawXGmqDIWpCiReF0ab" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 3", date: "05/26/2023", id: "1wtpffXwRkWwSfB5dzXTlHDDOUSIiyEWb" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 4", date: "05/26/2023", id: "119up4SgyO2OpiZQ9c_w8jCp7Td0Kt8UL" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 1", date: "05/25/2023", id: "1AOeBwaRbngL7pbrDPqb5jG1U1NkukvFH" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 2", date: "05/25/2023", id: "1wsZQNp77psnMo_8iCMtLIq19EesvHef9" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 3", date: "05/25/2023", id: "1BKpgkkGGxj-vO7K53EgLZHkZ_RR3haaq" },
-      { title: "West Side Jazz Club", location: "Ula SF - Set 4", date: "05/25/2023", id: "1YvWkERAdpXE-iBQVzgp9YScwyYegtMAd" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 2", date: "5/26/2023", id: "1Qt_6oPJ6XeMwiXawXGmqDIWpCiReF0ab" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 3", date: "5/26/2023", id: "1wtpffXwRkWwSfB5dzXTlHDDOUSIiyEWb" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 4", date: "5/26/2023", id: "119up4SgyO2OpiZQ9c_w8jCp7Td0Kt8UL" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 1", date: "5/25/2023", id: "1AOeBwaRbngL7pbrDPqb5jG1U1NkukvFH" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 2", date: "5/25/2023", id: "1wsZQNp77psnMo_8iCMtLIq19EesvHef9" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 3", date: "5/25/2023", id: "1BKpgkkGGxj-vO7K53EgLZHkZ_RR3haaq" },
+      { title: "West Side Jazz Club", location: "Ula SF - Set 4", date: "5/25/2023", id: "1YvWkERAdpXE-iBQVzgp9YScwyYegtMAd" },
       { title: "The Cody Sisters", location: "Amado's SF", date: "5/24/2023", id: "18Q_gKk0o6w5qShoOBK83kCQuLzx9a5oj" },
       { title: "Sunday Bluegrass Jam", location: "The Lucky Horseshoe", date: "5/21/2023", id: "1K1zMjTtyyYtygLkR-Rw6jP3IP_5FF-NP" },
       {
