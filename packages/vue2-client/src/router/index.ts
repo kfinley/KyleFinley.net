35119614f9696fdaccd13f9440c823e2e791b3ea
Kyle Finley <kyle@kylefinley.net>
2023-11-27 12:47:27 -0800

    update: refactor router.beforeEach. fix login page

--- packages/vue2-client/src/router/index.ts
+++ packages/vue2-client/src/router/index.ts
@@ -284,6 +284,7 @@ export const createRouter = async () => {
       case "Travel":
       case "News":
       case "Contact":
+      case "Login":
         return (await import(`../views/${pathParts[0]}.json`)).default
       default: {
         if (pathParts.length === 1) {
@@ -298,43 +299,12 @@ export const createRouter = async () => {
 
   // This callback runs before every route change, including on page load.
   router.beforeEach((to, from, next) => {
-    console.log('router.beforeEach', to, from);
-
-    // This goes through the matched routes from last to first, finding the closest route with a title.
-    // e.g., if we have `/some/deep/nested/route` and `/some`, `/deep`, and `/nested` have titles,
-    // `/nested`'s will be chosen.
-    const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
-
-    // Find the nearest route element with meta tags.
-    const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-
-    const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-
-    // If a route with a title was found, set the document (page) title to that value.
-    if (nearestWithTitle) {
-      document.title = nearestWithTitle.meta.title;
-    } else if (previousNearestWithMeta) {
-      document.title = previousNearestWithMeta.meta.title;
-    }
-
-    // Remove any stale meta tags from the document using the key attribute we set below.
-    // Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => {
-    //   if (el.parentNode) {
-    //     el.parentNode.removeChild(el)
-    //   }
-    // });
-
-    // If nearest with meta not found then load it from corresponding meta json file
-    if (nearestWithMeta == undefined) {
-
-      console.log('nearestWithMeta not found');
-
+    try {
       getMetaData(to.name as string).then((meta) => {
         document.title = meta.title
 
         meta.metaTags.map((tagDef: any) => {
           let tag = document.querySelector((`meta[${Object.keys(tagDef)[0]}='${tagDef[Object.keys(tagDef)[0]]}']`))
-          console.log(`meta[${Object.keys(tagDef)[0]}='${tagDef[Object.keys(tagDef)[0]]}']`, tag);
 
           if (!tag) {
             tag = document.createElement('meta');
@@ -342,46 +312,17 @@ export const createRouter = async () => {
           }
 
           Object.keys(tagDef).forEach(key => {
-            tag.setAttribute(key, tagDef[key]);
+            tag!.setAttribute(key, tagDef[key]);
           });
-
-          // We use this to track which meta tags we create so we don't interfere with other ones.
-          //tag.setAttribute('data-vue-router-controlled', '')
 
         });
         console.log('done w article metas');
         next();
-      })
-    }
-    else {
-
-      console.log('nearest with meta found');
-
-      // Turn the meta tag definitions into actual elements in the head.
-      nearestWithMeta.meta.metaTags.map((tagDef: any) => {
-        let tag = document.querySelector((`meta[${Object.keys(tagDef)[0]}='${tagDef[Object.keys(tagDef)[0]]}']`))
-        console.log(`meta[${Object.keys(tagDef)[0]}='${tagDef[Object.keys(tagDef)[0]]}']`, tag);
-
-        if (!tag) {
-          tag = document.createElement('meta');
-          document.head.appendChild(tag)
-        }
-
-        Object.keys(tagDef).forEach(key => {
-          tag.setAttribute(key, tagDef[key]);
-        });
-
-        // We use this to track which meta tags we create so we don't interfere with other ones.
-        //tag.setAttribute('data-vue-router-controlled', '');
-
-        // return tag;
-      })
-      // Add the meta tags to the document head.
-      // .forEach((tag: any) => document.head.appendChild(tag));
-      console.log('calling next');
+      });
+    } catch (e) {
+      console.log(e);
       next();
     }
-
   });
 
   router.afterEach((to, from) => {
