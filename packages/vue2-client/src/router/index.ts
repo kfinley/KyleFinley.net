4c4eece98d6877a571535a6750af53ad6875d25e
Kyle Finley <kyle@kylefinley.net>
2023-11-26 18:04:53 -0800

    update: testing meta image change

--- packages/vue2-client/src/router/index.ts
+++ packages/vue2-client/src/router/index.ts
@@ -238,6 +238,7 @@ export const createRouter = async () => {
       routes.push({
         path: `/${article}`,
         component: RouterLayout,
+        meta: viewsMeta[articleJson] ? (await viewsMeta[articleJson]() as any).default : { allowAnonymous: true },
         children: [
           {
             path: '',
@@ -247,7 +248,7 @@ export const createRouter = async () => {
                 path: '',
                 name: article,
                 component: articleComponent,
-                meta: viewsMeta[articleJson] ? (await viewsMeta[articleJson]() as any).default : { allowAnonymous: true }
+
               }
             ],
           }
