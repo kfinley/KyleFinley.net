cd876944734a791b2580d4b6c20cb813c3ddad07
Kyle Finley <kyle@kylefinley.net>
2024-02-01 18:05:45 +0000

    update: sheets

--- packages/songbook/src/commands/sheetsCommand.ts
+++ packages/songbook/src/commands/sheetsCommand.ts
@@ -9,7 +9,7 @@ export default abstract class SheetsCommand {
 
   protected api(id: string, sheet: string, range?: string) {
 
-    const key = 'AIzaSyC3Zd9-LCS6YWSkvA8mfpc-bo03o8UsHzA'; //TODO: process.env.SHEETS_API_KEY as string;
+    const key = process.env.GOOGLE_SHEETS_API_KEY as string; //TODO: move this to a config class
 
     let rangeSlug = '';
 
