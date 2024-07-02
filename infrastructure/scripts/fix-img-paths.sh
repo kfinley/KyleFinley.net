6f2f271b24cd4596bae074d190d1f3af63a71e6d
Kyle Finley <kyle@kylefinley.net>
2024-07-02 15:21:47 +0000

    updated: nested article image path fix

--- infrastructure/scripts/fix-img-paths.sh
+++ infrastructure/scripts/fix-img-paths.sh
@@ -8,5 +8,9 @@ for file in packages/vue2-client/dist/assets/*; do
 done
 
 for file in packages/vue2-client/dist/assets/*; do
+    sed -i 's/..\/..\/..\/..\/..\/media/https:\/\/d2f9ju3ov86hvq.cloudfront.net/g' $file
+done
+
+for file in packages/vue2-client/dist/assets/*; do
     sed -i 's/..\/..\/..\/..\/media/https:\/\/d2f9ju3ov86hvq.cloudfront.net/g' $file
 done
